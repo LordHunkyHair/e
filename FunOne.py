5e11a6c97aa1cf769da5fda32b1d934ce0b7070c
LordHunkyHair <lohmeyerjake12@gmail.com>
2024-06-12 11:22:19 -0500

    Add files via upload

---
+++ FunOne.py
@@ -0,0 +1,16 @@
+print ("Hello World")
+
+#Set Variables
+t = 0
+x = 0
+
+#Run for loop
+for t in range(0,1000000000):
+
+  x = x+1
+  t = t+1
+
+#If statement
+if x == 1000000000:
+
+  print("My name is Jacob Lohmeyer")
