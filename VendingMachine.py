af75f98ba761717315b84ddbb1ea069a9904b609
LordHunkyHair <lohmeyerjake12@gmail.com>
2024-06-15 10:41:48 -0500

    Add files via upload

---
+++ VendingMachine.py
@@ -0,0 +1,15 @@
+#create a program to ask for payment for vending machine items
+def main():
+    due = 50
+    while due > 0:
+        try:
+            print(f"Please pay {due} in coins:")
+            payment = int(input("Please enter your coin payment in numbers of cents: "))
+            if payment == 1 or payment == 5 or payment == 10 or payment == 25:
+                due = due - payment
+        except:
+            continue
+    if due < 0:
+        print(f"You are owed {due*-1} cents")
+
+main()
