7d8be55d13bc526c0192457f743a012561754dc0
LordHunkyHair <lohmeyerjake12@gmail.com>
2024-06-20 11:47:22 -0500

    Add files via upload

--- AddGame.py
+++ AddGame.py
@@ -2,23 +2,58 @@ import random
 def rand(start,stop):
     number1 = random.randint(start, stop)
     number2 = random.randint(start, stop)
-    total = number1 + number2
-    int(input(f"{number1} + {number2} = "))
-
-    return number1, number2
+    correct = 0
+    incorrect1 = 0
+    incorrect2 = 0
+    while True:
+        try:
+            total = number1 + number2
+            answer = int(input(f"{number1} + {number2} = "))
+            if answer == total:
+                print("Correct!")
+                correct += 1
+                break
+            elif answer != total:
+                incorrect1 += 1
+                if incorrect1 == 3:
+                    print(f"Incorrect! The answer was {total}")
+                    incorrect2 +=1
+                    break
+                print("Incorrect!")
+                continue
+        except:
+            continue
+    return correct
 
 def main():
-    difficulty = int(input("What difficulty do you want? 1, 2, or 3?: "))
-    if difficulty == 1:
-        start = 1
-        stop = 9
-    if difficulty == 2:
-        start = 10
-        stop = 99
-    if difficulty == 3:
-        start = 100
-        stop = 999
-    
-    rand(start,stop)
-
+    correctomundo = 0
+    while True:
+        try:
+            difficulty = int(input("What difficulty do you want? 1, 2, or 3?: "))
+            if difficulty == 1:
+                start = 1
+                stop = 9
+            elif difficulty == 2:
+                start = 10
+                stop = 99
+            elif difficulty == 3:
+                start = 100
+                stop = 999
+            else:
+                continue
+            while True:
+                try:
+                    questions = int(input("How many questions do you want between 3 through 10: "))
+                    if questions != 3 and questions != 4 and questions != 5 and questions != 6 and questions != 7 and questions != 8 and questions != 9 and questions != 10:
+                        continue
+                    for amount in range (questions):
+                        correct = rand(start,stop)
+                        correctomundo += correct
+                    print(f"You got {(correctomundo/questions)*100:.2f}% correct")
+                    break
+                except:
+                    continue
+        except:
+            continue
+        break
 main()
--- AlmostGame.py
+++ AlmostGame.py
@@ -1,15 +1,32 @@
 def main():
     map = [
-        ["[] ", "[] ", "[] "],
-        ["[] ", "[] ", "[] "],
-        ["[] ", "[] ", "[] "]
+        ["[ ]", "[ ]", "[ ]"],
+        ["[ ]", "[ ]", "[ ]"],
+        ["[ ]", "[ ]", "[ ]"]
     ]
-    x = 2
-    y = 1
-    print(map[y][x])
-    for row in map:
-        for column in row:
-            print(map[y][x])
-main()
+    x = 0
+    y = 0
+    player_x = 0
+    player_y = 0
+    move = str(input())
+    if move == "w":
+        player_y -= 1
+    if move == "a":
+        player_x -= 1
+    if move == "s":
+        player_y += 1
+    if move == "d":
+        player_x += 1
+    player_position = [player_y][player_x]
+    player = "P"
+    map_player = list(map)
+    map_player[player_y][player_x] = f"[{player}]"
 
-"[] [] [] "
+    #draw map
+    for y in map:
+        draw = ""
+        for x in y:
+            draw += x
+        print(f"{draw}")
+    
+main()
