a5c6d3ca1ab2303a9c394f5befdd4ec9e59831f6
LordHunkyHair <lohmeyerjake12@gmail.com>
2024-06-23 13:41:22 -0500

    Add files via upload

--- AlmostGame.py
+++ AlmostGame.py
@@ -1,7 +1,10 @@
+import random
 map = [
-        ["[ ]", "[ ]", "[ ]"],
-        ["[ ]", "[ ]", "[ ]"],
-        ["[ ]", "[ ]", "[ ]"]
+        ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+        ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+        ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+        ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]"],
+        ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]"]
     ]
 def draw_map():
     #draw map
@@ -14,40 +17,54 @@ def draw_map():
 def main():
     x = 0
     y = 0
+    z = 0
     player_x = 0
     player_y = 0
     old_player_x = 0
     old_player_y = 0
+    map_player = list(map)
     player_position = [player_y][player_x]
     player = "P"
-    map_player = list(map)
     map_player[player_y][player_x] = f"[{player}]"
+    enemy_x = 0
+    enemy_y = 4
+    old_enemy_x = 0
+    old_enemy_y = 0
+    map_enemy = list(map)
+    enemy_position = [enemy_y][enemy_x]
+    enemy = "E"
+    map_enemy[enemy_y][enemy_x] = f"[{enemy}]"
     #draw map
+    #player movement and enemy movement
     draw_map()
-    move = input(f"Please put in you movement: ")
-    if move == "w":
-        old_player_y = player_y
-        player_y -=1
-        map_player[player_y][player_x] = f"[{player}]"
-        map_player[old_player_y][player_x] = f"[ ]"
-        draw_map()
-    if move == "a":
-        old_player_x = player_x
-        player_x -=1
-        map_player[player_y][player_x] = f"[{player}]"
-        map_player[player_y][old_player_x] = f"[ ]"
-        draw_map()
-    if move == "s":
-        old_player_y = player_y
-        player_y +=1
-        map_player[player_y][player_x] = f"[{player}]"
-        map_player[old_player_y][player_x] = f"[ ]"
-        draw_map()
-    if move == "d":
-        old_player_x = player_x
-        player_x +=1
-        map_player[player_y][player_x] = f"[{player}]"
-        map_player[player_y][old_player_x] = f"[ ]"
-        draw_map()
+    while z == 0:
+        try:
+            move = input(f"Please put in you movement: ")
+            if move == "w":
+                old_player_y = player_y
+                player_y -=1
+                map_player[player_y][player_x] = f"[{player}]"
+                map_player[old_player_y][player_x] = f"[ ]"
+                draw_map()
+            if move == "a":
+                old_player_x = player_x
+                player_x -=1
+                map_player[player_y][player_x] = f"[{player}]"
+                map_player[player_y][old_player_x] = f"[ ]"
+                draw_map()
+            if move == "s":
+                old_player_y = player_y
+                player_y +=1
+                map_player[player_y][player_x] = f"[{player}]"
+                map_player[old_player_y][player_x] = f"[ ]"
+                draw_map()
+            if move == "d":
+                old_player_x = player_x
+                player_x +=1
+                map_player[player_y][player_x] = f"[{player}]"
+                map_player[player_y][old_player_x] = f"[ ]"
+                draw_map()
+        except:
+            continue
     
 main()
